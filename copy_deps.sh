de9887a54629f5ed31168649b0bf32c13e1f6938
CodeHz <CodeHz@outlook.com>
2020-08-18 20:05:55 +0800

    fix copy target

--- copy_deps.sh
+++ copy_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 deps=(/usr/lib/libgcc_s.so.1 /usr/lib/liblzma.so.5 /usr/lib/libunwind.so.8 /lib/ld-musl-x86_64.so.1)
 for file in ${deps[*]}; do
-    base=$(basename file);
+    base=$(basename "$file");
     cp "$file" "./dist/lib/$base";
     echo "copy $file to /lib/$base";
 done
