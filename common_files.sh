9fc7dba2b80b7cc44838f575ec4d213c48ed1752
CodeHz <CodeHz@outlook.com>
2021-02-02 04:52:00 +0800

    improve script

--- common_files.sh
+++ common_files.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eux
 cd dist
+mkdir -p ./{tmp,proc,dev,sys,var,run,etc}
+mkdir -p /usr/lib
+files+=(/usr/lib/lib{resolv,nss_{dns,files}}.so.2)
+for entry in ${files[@]}; do
+    install -D "$entry" ".$entry"
+    echo install ".$entry"
+done
 mkdir -p data
 mkdir -p root
 touch etc/localtime
--- copy_deps.sh
+++ copy_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eux
 cd dist
-../packager.sh -d lib64/wine/ntdll.so .
+../packager.sh bin/wine64 bin/wineserver lib64/wine/*.so
